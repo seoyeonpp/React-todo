7ff1fd5fadf65c68e3a380f4e371b9cea8c46602
박서연 <seoyeonpub@gmail.com>
2022-12-28 15:48:27 +0900

    react-hook-form으로 form 이벤트 수정

--- todo/src/ToDoList.tsx
+++ todo/src/ToDoList.tsx
@@ -1,29 +1,42 @@
 import React, { useState } from 'react';
 import { useForm } from "react-hook-form";
 
-// TODO: react-hook-form 을 사용하여 form 관리하기
-function ToDoList() {
-    const [todo, setTodo] = useState("");
-    const [toDoError, setToDoError] = useState("");
-    const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-        const { currentTarget: { value } } = event;
-        setTodo(value);
-        setToDoError('');
-    }
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        if (todo.length < 10) {
-            return setToDoError('Todo는 10글자보다 길어야 합니다.')
-        }
-        console.log(todo);
+// function ToDoList() {
+//     const [todo, setTodo] = useState("");
+//     const [toDoError, setToDoError] = useState("");
+//     const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+//         const { currentTarget: { value } } = event;
+//         setTodo(value);
+//         setToDoError('');
+//     }
+//     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//         event.preventDefault();
+//         if (todo.length < 10) {
+//             return setToDoError('Todo는 10글자보다 길어야 합니다.')
+//         }
+//         console.log(todo);
 
-        setTodo("");
-    }
+//         setTodo("");
+//     }
+//     return <div>
+//         <form onSubmit={onSubmit}>
+//             <input onChange={onChange} value={todo} placeholder='할일을 적으세요' />
+//             <button>추가! </button>
+//             {toDoError !== "" ? toDoError : null}
+//         </form>
+//     </div>
+// }
+
+// react hook form 으로 간단하게 작성하기
+function ToDoList() {
+    const { register, watch } = useForm();
+    console.log(watch())
     return <div>
-        <form onSubmit={onSubmit}>
-            <input onChange={onChange} value={todo} placeholder='할일을 적으세요' />
+        <form>
+            <input {...register("Email")} placeholder='Email' />
+            <input {...register("userName")} placeholder='Email' />
+            <input {...register("passWord")} placeholder='Email' />
             <button>추가! </button>
-            {toDoError !== "" ? toDoError : null}
         </form>
     </div>
 }
