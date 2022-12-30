8d16ed799e4b0ce9a8e0adcc2ccd5a11560f8a11
박서연 <seoyeonpub@gmail.com>
2022-12-30 16:06:17 +0900

    클릭한 category 노출

--- todo/src/components/ToDo.tsx
+++ todo/src/components/ToDo.tsx
@@ -1,12 +1,17 @@
-import { IToDo } from '../atoms';
+import { useSetRecoilState } from 'recoil';
+import { IToDo, toDoState } from '../atoms';
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+    const setToDos = useSetRecoilState(toDoState);
+    const onClick = (newCategory: IToDo["category"]) => {
+        console.log(newCategory)
+    }
     return (
         <li>
             {text}
-            <button>To Do</button>
-            <button>Doing</button>
-            <button>Done</button>
+            {category !== "TODO" && <button onClick={() => onClick("TODO")}>To Do</button>}
+            {category !== "DOING" && <button onClick={() => onClick("DOING")}>Doing</button>}
+            {category !== "DONE" && <button onClick={() => onClick("DONE")}>Done</button>}
         </li>
     )
 }
